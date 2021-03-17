5feb0497be21f8f82c3793c7fac35f92f034bb61
3loomin <tlsckdals8@naver.com>
2021-03-17 22:16:48 +0900

    Using  @RequestMapping instead of controller

--- webprj/src/main/java/com/New/web/controller/IndexController.java
+++ webprj/src/main/java/com/New/web/controller/IndexController.java
@@ -1,25 +1,28 @@
 package com.New.web.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
+@Controller
+public class IndexController {
 
-public class IndexController implements Controller{
-
-	@Override
-	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		System.out.println("index controller");
-		ModelAndView mv = new ModelAndView("root.index");
-		mv.addObject("data", "Hello Spring MVC");
-		//mv.setViewName("/WEB-INF/view/index.jsp");
-		
-		
-
-		
-		
-		return mv;
+	@RequestMapping("/index")
+	public void aaaa() {
+		System.out.println("asdfasdf");
 	}
+	
+//	@Override
+//	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+//		System.out.println("index controller");
+//		ModelAndView mv = new ModelAndView("root.index");
+//		mv.addObject("data", "Hello Spring MVC");
+//		//mv.setViewName("/WEB-INF/view/index.jsp");
+//		
+//		
+//
+//		
+//		
+//		return mv;
+//	}
 	
 }
